e0865504e206aeca2f96076c9eeef62e1e20765b
Flora Harvey <fharvey555@gmail.com>
2017-04-25 17:07:41 +0100

    Create array for requested pieces initially returning false

--- test/PiecesSpec.js
+++ test/PiecesSpec.js
@@ -2,11 +2,26 @@
 
 describe("Pieces", () => {
 
+  const dummyTorrent = {
+    info: {
+      pieces: {
+        length: 100
+      }
+    }
+  }
   const Pieces = require('../src/Pieces');
-  const pieces = new Pieces;
+  const pieces = new Pieces(dummyTorrent);
 
   it("is defined", () => {
     expect(pieces).toBeDefined();
+  });
+
+  it("creates an array for correct number of pieces", () => {
+    expect(pieces._requested.length).toEqual(5)
+  });
+
+  it("request array initially set to false for each piece", () => {
+    expect(pieces._requested[0]).toEqual(false)
   });
 
 
