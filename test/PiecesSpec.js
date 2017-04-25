46701ca8f25a72f909b8f0aa7dfbd4fa7f4a7c17
Flora Harvey <fharvey555@gmail.com>
2017-04-25 16:29:27 +0100

    Initial test for Pieces class passing

---
+++ test/PiecesSpec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe("Pieces", () => {
+
+  const Pieces = require('../src/Pieces');
+  const pieces = new Pieces;
+
+  it("is defined", () => {
+    expect(pieces).toBeDefined();
+  });
+
+
+});
