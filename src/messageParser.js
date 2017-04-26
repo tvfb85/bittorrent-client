a29118d2b9907280027316ae5f7b7597780e5756
nryn <narayanohanlon@gmail.com>
2017-04-26 16:58:19 +0100

    can now parse piece, cancel and request messages

--- src/messageParser.js
+++ src/messageParser.js
@@ -4,3 +4,20 @@ module.exports.isHandshake = (message) => {
   return message.length === 68 &&
          message.toString('utf8', 1, 20) === 'BitTorrent protocol';
 };
+
+module.exports.parse = (message) => {
+  const msgId = message.readInt8(4);
+
+  const payloadObj = {
+    index: message.readInt32BE(5),
+    begin: message.readInt32BE(9)
+  }
+
+  payloadObj[msgId === 7 ? 'block' : 'length'] = message.slice(13);
+
+  return {
+    size : message.readInt32BE(0),
+    id : msgId,
+    payload : payloadObj
+  }
+};
--- test/messageParserSpec.js
+++ test/messageParserSpec.js
@@ -2,28 +2,48 @@
 
 const messageParser = require('../src/messageParser')
 
+const handshakeMock = (() => {
+  const buffer = Buffer.alloc(68);
+  buffer.writeUInt8(19, 0); // pstrlen
+  buffer.write('BitTorrent protocol', 1); // pstr
+  buffer.writeUInt32BE(0, 20); // reserved pt1
+  buffer.writeUInt32BE(0, 24); // reserved pt2
+  buffer.write('this is the infohash', 28); // infohash
+  buffer.write('this is the peer__id', 48); // peerid
+  return buffer
+})();
+
+const pieceId = 7;
+const payload = "foo";
+const pieceMsgSize = 9 + payload.length;
+
+const pieceMock = (() => {
+  const buffer = Buffer.alloc(payload.length + 13);
+  buffer.writeUInt32BE(pieceMsgSize, 0); //len
+  buffer.writeUInt8(pieceId, 4); // id
+  buffer.writeUInt32BE(1, 5); // index
+  buffer.writeUInt32BE(0, 9); // begin
+  buffer.write(payload,13) // payload
+  return buffer;
+})();
+
 describe("MessageParser", () => {
-
-  const buildHandshakeMock = () => {
-    const buffer = Buffer.alloc(68);
-    // pstrlen
-    buffer.writeUInt8(19, 0);
-    // pstr
-    buffer.write('BitTorrent protocol', 1);
-    // reserved
-    buffer.writeUInt32BE(0, 20);
-    buffer.writeUInt32BE(0, 24);
-    // info hash
-    buffer.write('this is the infohash', 28);
-    // peer id
-    buffer.write('this is the peer__id', 48);
-    return buffer
-  }
-
-  const handshakeMock = buildHandshakeMock();
 
   it("can identify a handshake message", () => {
     expect(messageParser.isHandshake(handshakeMock)).toBe(true);
+  });
+
+  it("can identify a piece message", () => {
+    expect(messageParser.parse(pieceMock).id).toEqual(pieceId);
+  });
+
+  it("can tell us how big a message is", () => {
+    expect(messageParser.parse(pieceMock).size).toEqual(pieceMsgSize);
+  });
+
+  it("can allow us to access a message's payload", () => {
+    let payloadToCompare = messageParser.parse(pieceMock).payload.block.toString();
+    expect(payloadToCompare).toEqual(payload);
   });
 
 })
