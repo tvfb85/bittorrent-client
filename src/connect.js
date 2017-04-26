017013d0e25e055a0930e05e896e0677be2248be
Vicky Roberts <vicki_uk@hotmail.com>
2017-04-26 12:43:20 +0100

    Mocking socket

--- src/connect.js
+++ src/connect.js
@@ -1,8 +1,9 @@
 'use strict'
 const net = require('net');
 
-module.exports = peer => {
-  const socket = new net.Socket()
-  socket.connect(peer.port, peer.ip);
+module.exports = (peer, socket = new net.Socket()) => {
+  socket.connect(peer.port, peer.ip, () => {
+    socket.write('hello');
+  });
   socket.on('data', console.log);
 }
--- test/connectSpec.js
+++ test/connectSpec.js
@@ -18,7 +18,20 @@ describe("Connect", () => {
   it('should connect to the socket', () => {
     spyOn(net.Socket.prototype, "connect");
     connect(peer);
-    expect(net.Socket.prototype.connect).toHaveBeenCalledWith(peer.port, peer.ip);
+    expect(net.Socket.prototype.connect).toHaveBeenCalled();
+  });
+
+  it('should write to the socket', () => {
+
+    const dummySocket = new net.Socket();
+      dummySocket['connect'] = function() {
+        return dummySocket.write('hello');
+      };
+
+    spyOn(dummySocket, "write");
+
+    connect(peer, dummySocket);
+    expect(dummySocket.write).toHaveBeenCalled();
   });
 
 });
