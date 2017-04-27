f862a011c90b7a493e1c491c1d5794306551dc1d
Vicky Roberts <vicki_uk@hotmail.com>
2017-04-27 10:16:53 +0100

    Tests for buildHandshake (incomplete)

--- src/message.js
+++ src/message.js
@@ -1,6 +1,12 @@
-const Buffer = require('buffer').Buffer;
+'use strict';
 
-module.exports.buildHandshake = () => {
+const Buffer = require('buffer').Buffer;
+const bencode = require('bencode');
+const crypto = require('crypto');
+
+
+
+module.exports.buildHandshake = (torrent) => {
   const buf = Buffer.alloc(68);
   // pstrlen
   buf.writeUInt8(19, 0);
@@ -8,12 +14,26 @@ module.exports.buildHandshake = () => {
   buf.write('BitTorrent protocol', 1);
   // reserved
   buf.writeUInt32BE(0, 20);
-
   buf.writeUInt32BE(0, 24);
-
-  // info hash
-  // torrentParser.infoHash(torrent).copy(buf, 28);
-  // peer id
-  // util.genId().copy(buf, 48);
+  // reserve for info hash
+  infoHash(torrent).copy(buf, 28);
+  // reserve for peer id
+  createPeerId().copy(buf, 48);
   return buf;
+};
+
+function infoHash(torrent) {
+  const info = bencode.encode(torrent.info);
+  return crypto.createHash('sha1').update(info).digest();
+}
+
+function createPeerId() {
+  let id = null;
+
+  if (!id) {
+    id = crypto.randomBytes(20);
+    Buffer.from('-NVNF01-').copy(id, 0);
+  }
+
+  return id;
 };
--- test/messageSpec.js
+++ test/messageSpec.js
@@ -2,17 +2,62 @@
 
 const message = require('../src/message');
 const Buffer = require('buffer').Buffer;
+const bencode = require('bencode');
+const crypto = require('crypto');
+
+
 
 describe('message', () => {
 
+  let testBuf;
+  let torrent;
+
+  beforeEach(() => {
+    torrent = {
+          info: {
+            Dict: {
+              length: 1277987,
+              name: '<Buffer 66 6c 61 67 2e 6a 70 67>',
+              'piece length': 16384,
+              pieces: '<Buffer >'
+            }
+          }
+        }
+    testBuf = message.buildHandshake(torrent);
+  })
+
   it('should allocate 68 bytes to the buffer', () => {
-    const testBuf = message.buildHandshake();
     expect(testBuf.length).toEqual(68);
   });
 
-  it('should build a hanshake', () => {
-    const testBuf = message.buildHandshake();
+  it('should return a buffer', () => {
     expect(testBuf).toEqual(jasmine.any(Buffer));
   });
+
+  it('writes the pstrlen to the buffer', () => {
+    expect(testBuf[0]).toEqual(19);
+  });
+
+  it('writes the pstr to the buffer', () => {
+    const pstrChunk = testBuf.slice(1,20);
+    expect(pstrChunk.toString('utf8')).toEqual('BitTorrent protocol');
+  });
+
+  it('writes the info hash to the buffer', () => {
+    // message['infoHash'] = function(torrent) {
+    //   const info = bencode.encode(torrent.info);
+    //   return crypto.update(info).digest();
+    // };
+    const info = bencode.encode(torrent.info)
+    const torrentInfo = crypto.createHash('sha1').update(info).digest();
+    const infoHashChunk = testBuf.slice(28,48).toString('utf8');
+    const decodedInfoHashChunk = bencode.decode(infoHashChunk);
+    expect(decodedInfoHashChunk).toEqual(torrentInfo);
+  });
+
+  it('writes the peer ID to the buffer', () => {
+    const peerIdChunk = testBuf.slice(48,56);
+    expect(peerIdChunk.toString('utf8')).toEqual('-NVNF01-');
+  })
 
 })
