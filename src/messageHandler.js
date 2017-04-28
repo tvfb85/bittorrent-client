90aecbf409a222f5c1c471925c58f26a73eb153c
nryn <narayanohanlon@gmail.com>
2017-04-28 15:18:16 +0100

    piece handler is sort of feature complete

--- src/messageHandler.js
+++ src/messageHandler.js
@@ -19,7 +19,13 @@ module.exports.unchokeHandler = (socket, pieces, queue) => {
   connect.requestPiece(socket, pieces, queue);
 };
 
-module.exports.pieceHandler = (file, pieceData, torrent) => {
+module.exports.pieceHandler = (file, pieceData, torrent, socket, pieces, queue) => {
   const offset = pieceData.index * torrent.info['piece length'] + pieceData.begin;
   fs.write(file, pieceData.block, 0, pieceData.block.length, offset,() => {});
+  if (pieces.isComplete()) {
+    socket.end();
+    try { fs.closeSync(file); } catch(e) { }
+  } else {
+    connect.requestPiece(socket, pieces, queue)
+  }
 };
--- test/messageHandlerSpec.js
+++ test/messageHandlerSpec.js
@@ -10,40 +10,31 @@ const crypto = require('crypto');
 const connect = require('../src/connect');
 const fs = require('fs');
 
+let dummySocket = {
+  write: ()=>{},
+  end: ()=>{}
+};
+let torrent = {
+  info: {
+    length: 1277987,
+    name: '<Buffer 66 6c 61 67 2e 6a 70 67>',
+    'piece length': 16384,
+    pieces: '<Buffer >'
+  }
+};
+let testHandshake = message.buildHandshake(torrent);
+let interestedMessage;
+let dummyPieces = "dummyPieces";
+let dummyQueue = "dummyQueue";
+let dummyPieceResp = {
+  index: 1,
+  begin: 0,
+  block:  {
+    length: 10
+  }
+};
 
 describe("messageHandler", () => {
-
-  let dummySocket;
-  let testHandshake;
-  let torrent;
-  let interestedMessage;
-  let dummyQueue;
-  let dummyPieces;
-  let dummyPieceResp;
-
-  beforeEach(() => {
-    torrent = {
-      info: {
-        length: 1277987,
-        name: '<Buffer 66 6c 61 67 2e 6a 70 67>',
-        'piece length': 16384,
-        pieces: '<Buffer >'
-      }
-    };
-    testHandshake = message.buildHandshake(torrent);
-    dummySocket = {
-      write: function() {}
-    };
-    dummyPieces = "dummyPieces";
-    dummyQueue = "dummyQueue";
-    dummyPieceResp = {
-      index: 1,
-      begin: 0,
-      block:  {
-        length: 10
-      }
-    }
-  });
 
   it("sends an interested message if it receives a handshake", () => {
     spyOn(dummySocket, "write");
@@ -82,10 +73,56 @@ describe("messageHandler", () => {
     expect(connectSpy).toHaveBeenCalledWith(dummySocket, dummyPieces, dummyQueue);
   });
 
+});
+
+
+describe("pieceHandler", () => {
+
+  let pieces;
+
+  beforeEach(()=>{
+    pieces = {isComplete: ()=>{}};
+  });
+
   it('writes a block from a payload to the file', () => {
+    const piecesCompleteSpy = spyOn(pieces, 'isComplete').andReturn(false);
     const fileSpy = spyOn(fs, "write");
-    messageHandler.pieceHandler('filePath', dummyPieceResp, torrent);
+    const connectSpy = spyOn(connect, "requestPiece");
+    messageHandler.pieceHandler('filePath', dummyPieceResp, torrent, dummySocket, pieces, dummyQueue);
     expect(fileSpy).toHaveBeenCalledWith('filePath', dummyPieceResp.block, 0, dummyPieceResp.block.length, 16384, jasmine.any(Function));
+  });
+
+  it('requests the next piece in the queue', () => {
+    const piecesCompleteSpy = spyOn(pieces, 'isComplete').andReturn(false);
+    const fileSpy = spyOn(fs, "write");
+    const connectSpy = spyOn(connect, "requestPiece");
+    messageHandler.pieceHandler('filePath', dummyPieceResp, torrent, dummySocket, pieces, dummyQueue);
+    expect(connectSpy).toHaveBeenCalledWith(dummySocket, pieces, dummyQueue);
+  });
+
+  it('does not request another piece if file is complete', () => {
+    const piecesCompleteSpy = spyOn(pieces, 'isComplete').andReturn(true);
+    const fileSpy = spyOn(fs, "write");
+    const connectSpy = spyOn(connect, "requestPiece");
+    messageHandler.pieceHandler('filePath', dummyPieceResp, torrent, dummySocket, pieces, dummyQueue);
+    expect(connectSpy).not.toHaveBeenCalled();
+  });
+
+  it('ends socket connection if file is complete', () => {
+    const piecesCompleteSpy = spyOn(pieces, 'isComplete').andReturn(true);
+    const fileSpy = spyOn(fs, "write");
+    const socketSpy = spyOn(dummySocket, "end");
+    messageHandler.pieceHandler('filePath', dummyPieceResp, torrent, dummySocket, pieces, dummyQueue);
+    expect(socketSpy).toHaveBeenCalled();
+  });
+
+  it('closes file sync if file is complete', () => {
+    const piecesCompleteSpy = spyOn(pieces, 'isComplete').andReturn(true);
+    const fileSpy = spyOn(fs, "write");
+    const fileCloseSpy = spyOn(fs, "closeSync");
+    const socketSpy = spyOn(dummySocket, "end");
+    messageHandler.pieceHandler('filePath', dummyPieceResp, torrent, dummySocket, pieces, dummyQueue);
+    expect(fileCloseSpy).toHaveBeenCalledWith('filePath');
   });
 
 });
