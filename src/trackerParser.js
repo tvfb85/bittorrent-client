eae4117260e044d8352a97d7f7ed3bc50bdab822
nryn <narayanohanlon@gmail.com>
2017-04-30 16:10:57 +0100

    tracker parser can parse an announce response in to a peers object with an iterable value

--- src/trackerParser.js
+++ src/trackerParser.js
@@ -2,4 +2,18 @@
 
 module.exports.responseType = (resp) => {
   return (resp.readUInt32BE(0) === 0) ? 'connect' : 'announce';
-}
+};
+
+module.exports.parseAnnounceResp = (resp) => {
+  function groupPeerAddresses(allPeerData, groupSize = 6) {
+    let group = [];
+    for (let i = 0; i < allPeerData.length / groupSize; i ++) {
+      group.push(allPeerData.slice(i*groupSize, i*groupSize + 6));
+    }
+    return group;
+  };
+
+  return {
+    peers: groupPeerAddresses(resp.slice(20)).map(peer => { return {ip: peer.slice(0,4).join('.'), port: peer.readUInt16BE(4)}})
+  }
+};
--- test/trackerParserSpec.js
+++ test/trackerParserSpec.js
@@ -3,7 +3,7 @@ const trackerParser = require('../src/trackerParser');
 const crypto = require('crypto');
 
 
-describe('', () => {
+describe('trackerParser', () => {
 
   const connRespMock = (() => {
     const buffer = Buffer.alloc(16);
@@ -15,28 +15,65 @@ describe('', () => {
   })();
 
   const announceRespMock = (() => {
-    const buffer = Buffer.alloc(16);
+    const buffer = Buffer.alloc(38);
     buffer.writeUInt32BE(1, 0); // action
-    buffer.writeUInt32BE(0, 4); // transaction ID
-    buffer.writeUInt32BE(0, 8); // connection ID
-    buffer.writeUInt32BE(0, 12); // connection ID (2)
+    buffer.write("tran", 4); // transaction ID
+    buffer.write("intr", 8); // interval
+    buffer.write("leec", 12); // leechers
+    buffer.write("seed", 16); // seeders
+
+    buffer.writeUInt8(192, 20); // IP Address 1
+    buffer.writeUInt8(168, 21);
+    //buffer.writeUInt8(0, 22);
+    buffer.writeUInt8(1, 23);
+    buffer.writeUInt16BE(80, 24); // Port
+
+    buffer.writeUInt8(192, 26); // IP Address 2
+    buffer.writeUInt8(168, 27);
+    //buffer.writeUInt8(0, 28);
+    buffer.writeUInt8(4, 29);
+    buffer.writeUInt16BE(6881, 30); // Port
+
+
+    buffer.writeUInt8(192, 32); // IP Address 3
+    buffer.writeUInt8(168, 33);
+    //buffer.writeUInt8(0, 34);
+    buffer.writeUInt8(2, 35);
+    buffer.writeUInt16BE(90, 36); // Port
+
     return buffer;
   })();
 
-  it('identifies a connect message', () => {
-    expect(trackerParser.responseType(connRespMock)).toEqual('connect');
-  })
+  describe('responseType', () => {
 
-  it('does not respond with "announce" for a connect msg', () => {
-    expect(trackerParser.responseType(connRespMock)).not.toEqual('announce');
-  })
+    it('identifies a connect message', () => {
+      expect(trackerParser.responseType(connRespMock)).toEqual('connect');
+    });
 
-  it('identifies a announce message', () => {
-    expect(trackerParser.responseType(announceRespMock)).toEqual('announce');
-  })
+    it('does not respond with "announce" for a connect msg', () => {
+      expect(trackerParser.responseType(connRespMock)).not.toEqual('announce');
+    });
+    
+    it('identifies a announce message', () => {
+      expect(trackerParser.responseType(announceRespMock)).toEqual('announce');
+    });
 
-  it('does not respond with "connect" for a announce msg', () => {
-    expect(trackerParser.responseType(announceRespMock)).not.toEqual('connect');
-  })
+    it('does not respond with "connect" for a announce msg', () => {
+      expect(trackerParser.responseType(announceRespMock)).not.toEqual('connect');
+    });
+
+  });
+
+  describe('parseAnnounceResp', () => {
+
+    it('is a function', () => {
+      expect(trackerParser.parseAnnounceResp(announceRespMock)).toEqual(
+        {
+          peers: [{ip: "192.168.0.1", port: 80},{ip: "192.168.0.4", port: 6881},{ip: "192.168.0.2", port: 90}]
+        }
+      )
+    });
+
+  });
 
 });
